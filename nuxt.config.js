09630edd5007c601a8f9730c296424576c6941da
Ashlyn <ashlyn@lilyx.io>
2021-12-28 21:09:11 -0700

    added vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    'nuxt-vite'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
