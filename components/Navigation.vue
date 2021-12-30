4dbba24fc05eec6eb3286920bcc98e59d0acb136
Tony Grimes <acidtone@tonygrimes.com>
2021-12-29 23:31:51 -0700

    added utility nav, house rules

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,11 +1,18 @@
 <template>
-  <nav>
-    <ul>
-      <li><nuxt-link to="/schedule">Schedule</nuxt-link></li>
-      <li><nuxt-link to="/assignments">Assignments</nuxt-link></li>
-      <li><nuxt-link to="/library">Library</nuxt-link></li>
-      <li><nuxt-link to="/live-stream">Live Stream</nuxt-link></li>
-      <li><nuxt-link to="/courses">Courses</nuxt-link></li>
-    </ul>
-  </nav>
+  <div>
+    <nav arial-label="Primary Navigation">
+      <ul>
+        <li><nuxt-link to="/schedule">Schedule</nuxt-link></li>
+        <li><nuxt-link to="/courses">Courses</nuxt-link></li>
+        <li><nuxt-link to="/assignments">Assignments</nuxt-link></li>
+        <li><nuxt-link to="/library">Library</nuxt-link></li>
+      </ul>
+    </nav>
+    <nav arial-label="Utility Navigation">
+      <ul>
+        <li><nuxt-link to="/house-rules">House Rules</nuxt-link></li>
+        <li><nuxt-link to="/house-rules">Live Stream</nuxt-link></li>
+      </ul>
+    </nav>
+  </div>
 </template>
---
+++ pages/house-rules.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>House Rules</h1>
+    <h2>Live Stream Schedule</h2>
+    <ul>
+      <li><time datetime="08:00">8am</time>: Morning session</li>
+      <li><time datetime="11:30">11:30pm</time>: Lunch</li>
+      <li><time datetime="12:30">12:30am</time>: Lab Time</li>
+    </ul>
+  </div>
+</template>
