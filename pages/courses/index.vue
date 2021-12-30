1eeab8192abc774c10757be81f69ef2e85328e79
Ashlyn <ashlyn@lilyx.io>
2021-12-29 17:58:17 -0700

    static version of courses list

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -2,13 +2,38 @@
   <div>
     <h2>Courses</h2>
     <ul>
-      <p>{{ $route.path }}</p>
       <li>
-        <nuxt-link to="courses/cpnt-201">cpnt-201</nuxt-link>
+        <nuxt-link to="courses/cpnt-200">CPNT-200</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="courses/cpnt-201">CPNT-201</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="courses/cpnt-260">CPNT-260</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="courses/cpnt-262">CPNT-262</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="courses/cpnt-265">CPNT-265</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="courses/dsgn-270">DSGN-262</nuxt-link>
       </li>
     </ul>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  created() {
+    this.$router.options.routes.forEach((route) => {
+      this.courses.push({ name: route.name, path: route.path });
+    });
+  },
+  data() {
+    return {
+      courses: [],
+    };
+  },
+};
 </script>
