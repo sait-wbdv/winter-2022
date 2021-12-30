b369f88377d8a03368ad9a8f7367f4bc84efa192
Tony Grimes <acidtone@tonygrimes.com>
2021-12-29 23:04:47 -0700

    tweaked main nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
   <nav>
     <ul>
-      <li><nuxt-link to="schedule">Schedule</nuxt-link></li>
-      <li><nuxt-link to="assignments">Assignments</nuxt-link></li>
-      <li><nuxt-link to="library">Library</nuxt-link></li>
-      <li><nuxt-link to="zoom">Zoom</nuxt-link></li>
-      <li><nuxt-link to="courses"></nuxt-link></li>
+      <li><nuxt-link to="/schedule">Schedule</nuxt-link></li>
+      <li><nuxt-link to="/assignments">Assignments</nuxt-link></li>
+      <li><nuxt-link to="/library">Library</nuxt-link></li>
+      <li><nuxt-link to="/live-stream">Live Stream</nuxt-link></li>
+      <li><nuxt-link to="/courses">Courses</nuxt-link></li>
     </ul>
   </nav>
 </template>
