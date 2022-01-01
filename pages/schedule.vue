17d2025def912bd40bd54434a996b9dc0078e9ae
Tony Grimes <acidtone@tonygrimes.com>
2022-01-01 13:53:15 -0700

    added concattenated list of lessons

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <p>Schedule</p>
-  </div>
+  <main>
+    <h1>Program Schedule</h1>
+    <ul>
+      <li v-for="lesson in lessons" :key="lesson.id">
+          {{ lesson.title }}
+      </li>
+    </ul>
+  </main>
 </template>
+<script>
+export default {
+  async asyncData({ $content }) {
+    const values = await Promise.all(
+      [
+        $content("cpnt-201/lessons").fetch(),
+        $content("dsgn-270/lessons").fetch(),
+        $content("cpnt-260/lessons").fetch(),
+        $content("cpnt-262/lessons").fetch(),
+        $content("cpnt-200/lessons").fetch(),
+        $content("cpnt-265/lessons").fetch()
+      ]
+      
+    )
+    const lessons = [].concat.apply([],values)
+
+    return {
+      lessons,
+    };
+  },
+};
+</script>
