d3f2b0082477e16a16e30591b3370aaf529a54d6
Tony Grimes <acidtone@tonygrimes.com>
2022-01-01 19:10:48 -0700

    commented out luxon use and added TODO

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -32,7 +32,8 @@ export default {
       return a - b;
     })
 
-    console.log(this.$luxon("2020-10-05T14:48:00.000Z"));
+    // TODO: why isn't vue-luxon working?
+    // console.log(this.$luxon("2020-10-05T14:48:00.000Z"));
 
     return {
       lessons,
