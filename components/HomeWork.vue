728e7b1ebb048763c582a07abe0cd2d073305afc
lilyx13 <ashlyn@lilyx.io>
2022-01-07 01:04:21 -0700

    lesson components initial commit

---
+++ components/HomeWork.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <!-- Create template for homework content -->
+      <!-- article -->
+        <!-- title  --> 
+        <!-- list of links-->
+        <!-- embedded videos at full screen + links, only links at <half screen size -->
+  </section>
+</template>
+
+<script>
+export default {
+    // load an array from the frontmatter
+    // use conditionals in the template to render different layouts depending on content supplied
+      // can use a conditional or a computed property to select the correct article layout this would access the prop
+  props: {
+    homeWork: {
+      type: Array
+    } 
+  }
+}
+</script>
---
+++ components/TermsList.vue
@@ -0,0 +1,16 @@
+<template>
+<article>
+<!--add title --> 
+<!--add list -->
+</article>
+</template>
+<script>
+// import the definition file 
+export default {
+  props: {
+    definitions: {
+      type: Array
+    }
+  }
+}
+</script>
