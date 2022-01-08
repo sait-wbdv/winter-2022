4ae188808435c6cf9c94152d620bc4c447d62fca
lilyx13 <ashlyn@lilyx.io>
2022-01-08 05:36:24 -0700

    mostly done building term search function

--- components/TermsList.vue
+++ components/TermsList.vue
@@ -1,8 +1,13 @@
 <template>
 <section>
-  <h3>{{ definitions.title }}</h3>
+  <h3> Terminology </h3>
   <dl>
-    <div v-for="def in sortedDefinitions" :key="def.id">
+    <div v-for="def in definitions" :key="def.id">
+      <dt>{{ def.term }}</dt>
+      <dd>{{ def.definition }}</dd>
+    </div>
+    <!-- brute force version -->
+    <div v-for="def in userDefs" :key="def.id">
       <dt>{{ def.term }}</dt>
       <dd>{{ def.definition }}</dd>
     </div>
@@ -10,26 +15,27 @@
 </section>
 </template>
 <script>
+  import definitions from '~/static/data/definitions.json'
 
+export default {
+  data() {
+   return {
+     definitions
+   }
+  },
   props: {
-    defRequested: {
-      type: {
-        type: Array
-      }
-    },
-    computed: {
-      sortedDefinitions() {
-        let output = [];
-        this.defRequested.forEach(term => {
-         this.globalDefinitions.forEach(def => {
-          if (term.term === def.term) {
-            output.push(def)
-          }
-         }) 
-        })
-        return output;
-      }
+   defReq: {
+     type: Array
+     // set required true when ready
+   },
+   userDefs: {
+     type: Array
+   }
+  },
+  computed: {
+    defSearch() {
     }
   }
-}
+};
+
 </script>
