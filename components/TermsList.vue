d6b8c126ae64e2ec2eab510b66d776e926acf245
lilyx13 <ashlyn@lilyx.io>
2022-01-08 16:05:39 -0700

    added work in progress code on termslist search

--- components/TermsList.vue
+++ components/TermsList.vue
@@ -1,39 +1,37 @@
 <template>
 <section>
-  <h3> Terminology </h3>
+  <h3> Terminology</h3>
   <dl>
-    <div v-for="def in definitions" :key="def.id">
+    <div v-for="def in defSearch" :key="def.id">
       <dt>{{ def.term }}</dt>
       <dd>{{ def.definition }}</dd>
     </div>
-    <!-- brute force version -->
-    <div v-for="def in userDefs" :key="def.id">
-      <dt>{{ def.term }}</dt>
-      <dd>{{ def.definition }}</dd>
-    </div>
+
   </dl>
 </section>
 </template>
 <script>
-  import definitions from '~/static/data/definitions.json'
+import definitions from '~/static/data/definitions.json'
 
 export default {
   data() {
-   return {
-     definitions
-   }
+    return {
+      definitions
+    }
   },
   props: {
-   defReq: {
-     type: Array
+   searchTerms: {
+     type: Array,
+     default: () => []
      // set required true when ready
-   },
-   userDefs: {
-     type: Array
    }
   },
   computed: {
+    // 
     defSearch() {
+      return this.definitions.filter(word => {
+        return this.searchTerms.indexOf(word.term) > -1
+      })
     }
   }
 };
