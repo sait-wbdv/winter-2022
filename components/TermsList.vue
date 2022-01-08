933b9fb995e625a134bb06996053972b5a3ffc8f
lilyx13 <ashlyn@lilyx.io>
2022-01-08 03:45:55 -0700

    initial sorted definitionn function

--- components/TermsList.vue
+++ components/TermsList.vue
@@ -1,15 +1,34 @@
 <template>
-<article>
-<!--add title --> 
-<!--add list -->
-</article>
+<section>
+  <h3>{{ definitions.title }}</h3>
+  <dl>
+    <div v-for="def in sortedDefinitions" :key="def.id">
+      <dt>{{ def.term }}</dt>
+      <dd>{{ def.definition }}</dd>
+    </div>
+  </dl>
+</section>
 </template>
 <script>
-// import the definition file 
-export default {
+
   props: {
-    definitions: {
-      type: Array
+    defRequested: {
+      type: {
+        type: Array
+      }
+    },
+    computed: {
+      sortedDefinitions() {
+        let output = [];
+        this.defRequested.forEach(term => {
+         this.globalDefinitions.forEach(def => {
+          if (term.term === def.term) {
+            output.push(def)
+          }
+         }) 
+        })
+        return output;
+      }
     }
   }
 }
