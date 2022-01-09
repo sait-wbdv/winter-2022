ea246d6cf3a8bb62182a281c9242fb070c2f5782
Tony Grimes <acidtone@tonygrimes.com>
2022-01-09 15:29:55 -0700

    added virtual nest dir support

---
+++ pages/library/_.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main>
+      <h1>{{ page.title }}</h1>
+      <nuxt-content :document="page" />
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const page = await $content(`library/${params.pathMatch}`).fetch();
+      return {
+        page,
+      };
+    } catch (e) {
+      error("Page not found.");
+    }
+  },
+};
+</script>
