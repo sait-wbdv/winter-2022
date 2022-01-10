1207bc8428cc9f4e3601c9e96030ccb1dd0c20e1
Ashlyn <ashlyn@lilyx.io>
2022-01-09 18:43:44 -0700

    added simple styling to homework component

--- components/HomeWork.vue
+++ components/HomeWork.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <article v-for="topic in homeWork" :key="topic.id">
+  <section class="border-solid border-2 border-black rounded-2xl p-4 m-6 flex flex-col mx-auto">
+    <article v-for="topic in homeWork" :key="topic.id" class="my-1">
       <h3>{{ topic.title }}</h3>
       <ul>
         <li v-for="task in topic.tasks" :key="task.id">
