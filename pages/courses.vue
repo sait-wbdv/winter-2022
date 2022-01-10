31523cde1ae408a0c1fc0385c28c6a149c9478d8
Tony Grimes <acidtone@tonygrimes.com>
2022-01-09 22:09:12 -0700

    re-ordered courses

--- pages/courses.vue
+++ pages/courses.vue
@@ -4,22 +4,22 @@
       <h2>Courses</h2>
       <ul>
         <li>
-          <nuxt-link to="cpnt-200">CPNT-200</nuxt-link>
+          <nuxt-link to="cpnt-201">CPNT-201: Web Development Tools (Dev Ops)</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="cpnt-201">CPNT-201</nuxt-link>
+          <nuxt-link to="dsgn-270">DSGN-270: Design Fundamentals</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="cpnt-260">CPNT-260</nuxt-link>
+          <nuxt-link to="cpnt-260">CPNT-260: Web Page Fundamentals</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="cpnt-262">CPNT-262</nuxt-link>
+          <nuxt-link to="cpnt-262">CPNT-262: Client and Server Programming</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="cpnt-265">CPNT-265</nuxt-link>
+          <nuxt-link to="cpnt-200">CPNT-200: Content Management Systems</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="dsgn-270">DSGN-270</nuxt-link>
+          <nuxt-link to="cpnt-265">CPNT-265: Business of the Web</nuxt-link>
         </li>
       </ul>
     </main>
