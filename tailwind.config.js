9c14886e546f16eb0a2d7fe99c6cdae1b1f60817
Ashlyn <ashlyn@lilyx.io>
2022-01-09 18:20:24 -0700

    fixed typo on screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      xs: '348',
+      xs: '348px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
