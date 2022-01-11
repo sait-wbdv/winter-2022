dca800d56c6960516c6c398dc755580da4744455
Tony Grimes <acidtone@tonygrimes.com>
2022-01-11 07:39:59 -0700

    typo fix

--- pages/house-rules.vue
+++ pages/house-rules.vue
@@ -5,8 +5,8 @@
       <h2>Live Stream Schedule</h2>
       <ul>
         <li><time datetime="08:00">8am</time>: Morning session</li>
-        <li><time datetime="11:30">11:30pm</time>: Lunch</li>
-        <li><time datetime="12:30">12:30am</time>: Lab Time</li>
+        <li><time datetime="11:30">11:30am</time>: Lunch</li>
+        <li><time datetime="12:30">12:30pm</time>: Lab Time</li>
       </ul>
 
       <h2>Camera Policy</h2>
